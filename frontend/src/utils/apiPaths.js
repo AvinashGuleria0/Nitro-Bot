3675d1293b5939865c68f2f6dfb0a7e10a3e62c8
Avinash Guleria <avinashguleria1009@gmail.com>
2025-08-03 23:26:54 +0530

    done

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:9000";
+// export const BASE_URL = "http://localhost:9000";
+export const BASE_URL = "https://nitro-bot-0fsz.onrender.com/";
 
 export const API_PATHS = {
   AUTH: {
